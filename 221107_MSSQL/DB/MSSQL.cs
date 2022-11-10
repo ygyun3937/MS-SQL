47a0f79e156f4349f089693471816e3d722aa708
YG <YG@DESKTOP-29PU1UU>
2022-11-10 23:56:41 +0900

    MSSQL Module 추가

---
+++ 221107_MSSQL/DB/MSSQL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _221107_MSSQL.MSSQL
+{
+    public class MSSQL
+    {
+
+        public void Open()
+        {
+
+        }
+    }
+}
